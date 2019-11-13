a989cb0b9bdecf9b9808c155d753e417b4c650fe
Mariano Conti <nanexcool@gmail.com>
2019-11-13 02:45:32 -0300

    Fix

--- src/median.sol
+++ src/median.sol
@@ -41,7 +41,7 @@ contract MedianBATUSD is Median {
     }
 }
 
-contract MedianETHUSD is Median {
+contract MedianBTCUSD is Median {
     bytes32 public constant wat = "BTCUSD";
 
     function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
