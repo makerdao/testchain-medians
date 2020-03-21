0ec987388c53fecabbb1cf2d749587928d906fcf
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-03-21 10:23:58 -0300

    Add UDSCUSD

--- src/median.sol
+++ src/median.sol
@@ -106,3 +106,14 @@ contract MedianGNTUSD is Median {
         );
     }
 }
+
+contract MedianUSDCUSD is Median {
+    bytes32 public constant wat = "USDCUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
