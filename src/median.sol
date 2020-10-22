599dd9c4a4a0a477b14b7cff1830e911366d2b7b
Sam MacPherson <xylom44@gmail.com>
2020-10-22 18:23:38 -0400

    added YFI median

--- src/median.sol
+++ src/median.sol
@@ -216,3 +216,14 @@ contract MedianBALUSD is Median {
         );
     }
 }
+
+contract MedianYFIUSD is Median {
+    bytes32 public constant wat = "YFIUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
