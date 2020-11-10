688ada16a3bb74b3677b6faf716345bdadee9671
Sam MacPherson <xylom44@gmail.com>
2020-11-10 10:32:13 -0500

    added GUSD median (#10)

--- src/median.sol
+++ src/median.sol
@@ -227,3 +227,14 @@ contract MedianYFIUSD is Median {
         );
     }
 }
+
+contract MedianGUSDUSD is Median {
+    bytes32 public constant wat = "GUSDUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
