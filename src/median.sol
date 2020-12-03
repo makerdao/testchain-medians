5915b053b33b995899b6755da411968f71ec0b0f
Sam MacPherson <xylom44@gmail.com>
2020-12-03 08:13:45 -0500

    added RENBTC median

--- src/median.sol
+++ src/median.sol
@@ -238,3 +238,15 @@ contract MedianGUSDUSD is Median {
         );
     }
 }
+
+contract MedianRENBTCUSD is Median {
+    bytes32 public constant wat = "RENBTCUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
+
