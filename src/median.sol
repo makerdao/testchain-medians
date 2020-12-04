00083cb44bbd00a61ea7f5bb48cb0a05d4c4da98
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-12-04 22:36:20 +0000

    add UNI median

--- src/median.sol
+++ src/median.sol
@@ -238,3 +238,14 @@ contract MedianGUSDUSD is Median {
         );
     }
 }
+
+contract MedianUNIUSD is Median {
+    bytes32 public constant wat = "UNIUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
